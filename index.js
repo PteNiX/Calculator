d7530c485e5810e823076e80f598de318026ba99
Aliaksandr Ptsitsa <ptenix@gmail.com>
2022-07-08 21:27:07 +0300

    feat: add functionality for operators

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ let operator = "";
 let finish = false;
 
 let digit = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "."];
-let action = ["+", "-", "X", "/"];
+let action = ["+", "-", "x", "/"];
 
 let screen = document.querySelector(".screen");
 
@@ -27,11 +27,55 @@ AC.addEventListener("click", clearAll);
 
 document.querySelector(".buttons").onclick = (e) => {
   const key = e.target.innerHTML;
-  console.log(key);
 
   if (digit.includes(key)) {
-    a += key;
-    console.log(a, b, operator);
+    if (b === "" && operator === "") {
+      a += key;
+
+      screen.innerHTML = a;
+    } else if (a != "" && b != "" && finish) {
+      b = key;
+      finish = false;
+      screen.innerHTML = b;
+    } else {
+      b += key;
+      screen.innerHTML = b;
+    }
+  }
+
+  if (action.includes(key)) {
+    operator = key;
+    screen.innerHTML = operator;
+  }
+
+  //equal
+  if (key == "=") {
+    if (b == "") b = a;
+    switch (operator) {
+      case "+":
+        a = +a + +b;
+        break;
+
+      case "-":
+        a = +a - +b;
+        break;
+
+      case "x":
+        a = +a * +b;
+        break;
+
+      case "/":
+        if (b == 0) {
+          document.querySelector(".screen").innerHTML = "памылка";
+          a = "";
+          b = "";
+          operator = "";
+          return;
+        }
+        a = +a / +b;
+        break;
+    }
+    finish = true;
     screen.innerHTML = a;
   }
 };
